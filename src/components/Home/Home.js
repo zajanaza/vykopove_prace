a26316bc5d8a0139fa34062a255b6082f2d2cbd8
zajanaza <janazadrazilova@seznam.cz>
2023-08-04 14:20:43 +0200

    countMen a countWomen added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import operators from '../../operatorsData';
-import { PageContainer, OperatorList, OperatorItem, OperatorForm, Input, Button } from './HomeStyle';
+import { PageContainer, OperatorList, OperatorItem, OperatorForm, Input, Button, Buttons, TabButton, ShelterForm, TabButton2 } from './HomeStyle';
 import { useState, useRef } from "react";
 
 export default function Home() {
   const [listOfOperators, setListOfOperators] =useState(operators);
   const operatorsCount = useRef(operators.length);
+  const [activeTab, setActiveTab] = useState("list-of-operators");  
+  const [countMen, setCountMen] = useState (3);
+  const [countWomen, setCountWomen] = useState (2);
   const [newOperator, setNewOperator] = useState({
     id: (listOfOperators.length > 0 ? Math.max(...listOfOperators.map(operator => operator.id)) + 1 : 1),
     name:"",
@@ -34,6 +37,9 @@ export default function Home() {
     setListOfOperators((listOfOperators) => {
       return [...listOfOperators, newOperator];
     });
+    if (newOperator.gender === 'men') {
+      setCountMen(countMen+1);
+    } else setCountWomen(countWomen+1);
     operatorsCount.current++;
     const newOperatorGender = newOperator.gender;
     const updateOperator = {
@@ -43,61 +49,90 @@ export default function Home() {
       gender:newOperatorGender,      
     }
     setNewOperator(updateOperator); 
-    setValid(false);    
+    setValid(false);        
   };  
-  const handleDelete = (idToDel) => {
+
+  const handleDelete = (idToDel, genderToDel) => {
     setListOfOperators(listOfOperators.filter((operator) => operator.id !== idToDel));
-  };
+    if (genderToDel === 'men') {
+      setCountMen(countMen-1)
+    } else setCountWomen(countWomen-1);
+  }; 
+  
   return (
     <PageContainer>
-      <OperatorList name='operatorList'>
-        {listOfOperators.map((operator) => {
-          return(
-            <OperatorItem key={operator.id}>
-              {operator.name}  {operator.surname} - {operator.gender}
-              <button
-              style={{
-                  color: 'black',
-                  fontWeight: 'bolder',
-                  border: 2 + 'px solid #D9C0AE',
-                  borderRadius: 50 + '%',
-                  height: 25 + 'px', 
-                  width: 25 + 'px' 
-              }}
-              onClick={() => {handleDelete(operator.id)}}>
-              X
-              </button>
-            </OperatorItem>
-          )
-        })}
-      </OperatorList>   
-      <OperatorForm>
-        <Input
-          type='text'
-          placeholder='jmeno'
-          name='name'
-          value={newOperator.name}
-          onChange={handleChange} />       
-        <Input 
-          type='text'
-          placeholder='surname'
-          name='surname'
-          value={newOperator.surname}
-          onChange={handleChange}/>        
-        <Input 
-          type='radio'
-          name='gender'
-          value='men'                                     
-          onChange={handleChange}         
-          />MEN       
-        <Input 
-          type='radio'
-          name='gender'
-          value='women'           
-          onChange={handleChange}         
-          />WOMEN  
-        <Button disabled={!valid} onClick={handleAdd}>Add Employee</Button>
-      </OperatorForm>
+      <Buttons>
+        <TabButton name="list-of-operators" data-active={activeTab} onClick={() => setActiveTab ('list-of-operators')}>List of Employees</TabButton>
+        <TabButton name='shelter-storage' data-active={activeTab} onClick={() => setActiveTab('shelter-storage')}>Tasks</TabButton>
+      </Buttons>
+      {(activeTab === 'list-of-operators') &&
+      <>
+        <OperatorList name='operatorList'>
+          {listOfOperators.map((operator) => {
+            return(
+              <OperatorItem key={operator.id}>
+                {operator.name}  {operator.surname} - {operator.gender}
+                <button
+                style={{
+                    color: 'black',
+                    fontWeight: 'bolder',
+                    border: 2 + 'px solid #D9C0AE',
+                    borderRadius: 50 + '%',
+                    height: 25 + 'px', 
+                    width: 25 + 'px' 
+                }}
+                onClick={() => {handleDelete(operator.id, operator.gender)}}>
+                X
+                </button>
+              </OperatorItem>
+            )
+          })}
+        </OperatorList>   
+        <OperatorForm>
+          <Input
+            type='text'
+            placeholder='jmeno'
+            name='name'
+            value={newOperator.name}
+            onChange={handleChange} />       
+          <Input 
+            type='text'
+            placeholder='surname'
+            name='surname'
+            value={newOperator.surname}
+            onChange={handleChange}/>        
+          <Input 
+            type='radio'
+            name='gender'
+            value='men'                                     
+            onChange={handleChange}         
+            />MEN       
+          <Input 
+            type='radio'
+            name='gender'
+            value='women'           
+            onChange={handleChange}         
+            />WOMEN  
+          <Button disabled={!valid} onClick={handleAdd}>Add Employee</Button>
+        </OperatorForm>
+      </>}
+      {activeTab === 'shelter-storage' &&
+      <>
+        <h3 style={{color: '#73453F'}}>PLANNING EXCAVATION WORKS</h3>
+        <p>MEN: {countMen} </p>
+        <p>WOMEN: {countWomen} </p>
+        <ShelterForm>
+          <Input 
+          type="number"
+          min="0"
+          placeholder="Enter meters"/>          
+          <Input 
+          type="number"
+          min="0"
+          placeholder="Enter hours"/>  
+          <TabButton2>Work planning</TabButton2>        
+        </ShelterForm>
+      </>}  
     </PageContainer>  
   )  
 }
--- src/components/Home/HomeStyle.js
+++ src/components/Home/HomeStyle.js
@@ -50,3 +50,38 @@ color:#D9C0AE;
 border:none;
 border-radius: 5px;
 `;
+
+export const Buttons = styled(OperatorForm)`          
+  margin: 30px 0;
+  height: 40px;
+`;
+
+export const TabButton = styled.button`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 48%;
+  border: 1px solid white;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  background-color: transparent;
+  ${(props) =>{
+    if (props.name === props['data-active']){              //zmena pozadi tlacitka pri aktivaci vyberu
+      return `
+        background-color: black`;
+    }
+  }}
+`;
+
+export const ShelterForm = styled(OperatorForm)`          
+
+    flex-direction: column;
+
+`;     
+
+export const TabButton2 = styled(TabButton)`
+
+`;
